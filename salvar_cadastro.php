30fb1aba560f2ef8396de1cfa9ee8d7f8e541cf3
Jonatha io <161509156+Jonatha231@users.noreply.github.com>
2025-11-28 10:43:57 -0300

    Add files via upload

--- salvar_cadastro.php
+++ salvar_cadastro.php
@@ -14,6 +14,7 @@ if ($_POST) {
     
 // Verifica se a consulta encontrou algum registro com o mesmo e-mail.
 // rowCount() retorna o número de linhas retornadas pela query.
+// O query verifica se já existe um usuário com o e-mail fornecido.
 // Se for maior que 0, significa que o e-mail já existe no banco.
     if ($resultado->rowCount() > 0) {
         echo "<script>alert('Este e-mail já está cadastrado!'); window.history.back();</script>";
